90d81a6e167b2a731c9dba809f596d62e5cf72d6
Ibnu Jakaria <ibnujakaria@outlook.com>
2018-03-04 20:27:18 +0700

    mbuh kenapa cors error gara2 masalah dependency

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -6,12 +6,31 @@ use Closure;
 
 class cors {
 
-    public function handle($request, Closure $next) {
-        return $next($request)
-                        ->header('Access-Control-Allow-Origin', '*')
-                        ->header('Access-Control-Allow-Methods', '*')
-                        ->header('Access-Control-Allow-Headers', 'Accept, Content-Type,X-CSRF-TOKEN')
-                        ->header('Access-Control-Allow-Credentials', 'true');
+  public function handle($request, Closure $next) {
+    $response = $next($request);
+    $IlluminateResponse = 'Illuminate\Http\Response';
+    $SymfonyResopnse = 'Symfony\Component\HttpFoundation\Response';
+    $headers = [
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, PATCH, DELETE',
+      'Access-Control-Allow-Headers' => 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Authorization , Access-Control-Request-Headers',
+    ];
+
+    if($response instanceof $IlluminateResponse) {
+      foreach ($headers as $key => $value) {
+        $response->header($key, $value);
+      }
+      return $response;
     }
+
+    if($response instanceof $SymfonyResopnse) {
+      foreach ($headers as $key => $value) {
+        $response->headers->set($key, $value);
+      }
+      return $response;
+    }
+
+    return $response;
+  }
 
 }
